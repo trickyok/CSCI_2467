0e94a4d2d8638ee73419d90b6deccf976412c374
Gage Farmer <trickyok@users.noreply.github.com>
2021-10-07 15:59:02 -0400

    p u s h
    p u l l

---
+++ inClassExamples/src/edu/cscc/Dog.java
@@ -0,0 +1,20 @@
+package edu.cscc;
+
+public class Dog {
+
+    public String name, breed;
+    public int age;
+
+    public String getName() { return this.name; }
+
+    public String getBreed() { return this.breed; }
+
+    public int getAge() { return this.age; }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setBreed(String breed) { this.breed = breed; }
+
+    public void setAge(int age) { this.age = age; }
+
+}
